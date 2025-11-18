da71c99b035230fda157b90f9e4e99324f3a676e
nived-s <nivedsreenilayam@gmail.com>
2025-11-18 10:23:00 +0000

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: "url('/bg_desk.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/*  monitor screen div here */}
+    </main>
+  );
+}
